d7d28d8df8878cd11e8057cd12e3d29222e3ca9f
wuh3 <wuh3@rose-hulman.edu>
2019-03-04 09:38:27 -0500

    CSSE 120 L1 M2

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -87,7 +87,7 @@ print("I'm Mike")
 print(3607*34227)
 ###############################################################################
 #
-# TODO: 5.
+# done
 #   Look at the list of files in the Project window (to the left).
 #   Note that this file:
 #      m2_todo_and_commit_push.py
